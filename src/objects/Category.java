6bf4f7901899544241fffd9322d27babd7c68922
vanyatoneva <vanyattoneva@gmail.com>
2016-08-27 13:47:28 +0300

    del

--- src/objects/Category.java
+++ src/objects/Category.java
@@ -1,5 +1,29 @@
 package objects;
 
-public class Category {
+import java.util.ArrayList;
 
+public class Category {
+	private String name;
+	private ArrayList<Picture> pics = new ArrayList<Picture>();
+	
+	Category(String name){
+		this.name = name;
+	}
+	
+	public void addPic(Picture pic){
+		pics.add(pic);
+	}
+	
+	public ArrayList<Picture> getFresh(){
+		ArrayList<Picture> fresh = new ArrayList<Picture>();
+		for(int i = pics.size(); i >= 0; i--){
+			fresh.add(pics.get(i));
+		}
+		return fresh;
+	}
+	
+	public ArrayList<Picture> getHot(){
+		ArrayList<Picture> hot = new ArrayList<Picture>();
+		return hot;
+	}
 }
